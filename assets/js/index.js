e1ea6c650e4416d9ef5dea1ead072537765480ff
josemedina1 <medinavera.jose@gmail.com>
2023-11-08 23:42:14 -0300

    validacion de campos vacios js

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,24 @@ const form = document.querySelector('.form-weather');
 const nameCity = document.querySelector('.city');
 const nameCountry = document.querySelector('.country');
 
+const alertNull = document.getElementById('alertnull');
 
-form.addEventListener('submit', (e)=>{
-//Prevenir refrescar la pagina del formulario al dar click en el boton
+form.addEventListener('submit', (e) => {
+    //Prevenir refrescar la pagina del formulario al dar click en el boton
     e.preventDefault();
-//Mostrar por consola el valor ingresado en inputs
-    console.log('Ciudad: ',nameCity.value)
-    console.log('PAIS: ',nameCountry.value)
+
+    if (nameCity.value === '' || nameCountry.value === '') {
+        alertNull.innerHTML = `<div class="alert alert-danger" role="alert">
+        Ambos campos son obligatorios!
+        </div>`;
+    }
+
+    //Mostrar por consola el valor ingresado en inputs
+    console.log('Ciudad: ', nameCity.value)
+    console.log('PAIS: ', nameCountry.value)
+})
+
+// Quitar alerta nulos al dar click en el formulario
+form.addEventListener('click',(e)=>{
+    alertNull.innerHTML = '';
 })
